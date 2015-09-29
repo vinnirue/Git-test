cc970e259250f706baf45de365f3909d5d6877aa
daVinci <daVinci@10.34.28.63>
2015-09-29 13:23:15 +0300

    add2

--- Git-test-project/src/ru/vin/test/Main.java
+++ Git-test-project/src/ru/vin/test/Main.java
@@ -3,7 +3,7 @@ package ru.vin.test;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello from Git Hub!!!");
+		System.out.println("Hello from Git Hub!!");
 
 	}
 
